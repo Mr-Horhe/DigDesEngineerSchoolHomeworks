1abf79b706a97157dff31dae6db060b94d51c321
Sergey Morozov <82180346+Mr-Horhe@users.noreply.github.com>
2023-05-23 18:02:58 +0300

    Add files via upload

---
+++ c# topic/MyAbstract/MyAbstract/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace MyAbstract
+{
+    abstract class Cat
+    {
+        public string name;
+        public string color;
+
+        public Cat()
+        {
+            name = "Barsic";
+            color = "gray";
+        }   
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var temp = typeof(Cat);
+            var abs = typeof(RuntimeTypeHandle).GetMethod("Allocate", BindingFlags.Static | BindingFlags.NonPublic);
+
+            var absObj = (Cat)abs.Invoke(null, new object[] { typeof(Cat) });
+
+            typeof(Cat).GetConstructor(Type.EmptyTypes).Invoke(absObj, new object[0]);
+
+            Console.WriteLine(absObj.name + " is "+absObj.color+" cat");
+        }
+    }
+}
---
+++ c# topic/MyExe/MyExe/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLib;
+using System.Reflection;
+
+namespace MyExe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inpPath = "D:\\c++\\Test\\Test\\test.txt";
+            string outPath = "D:\\c++\\Test\\Test\\out.txt";
+
+            string[]inptext = File.ReadAllLines(inpPath);
+
+            Class1 class1 = new Class1();
+            var myMethod = typeof(Class1).GetMethod("myDic", BindingFlags.Instance | BindingFlags.NonPublic);
+            Dictionary<string, int> myDict = (Dictionary<string, int>)myMethod.Invoke(class1, new object[] { inptext });
+
+            using (StreamWriter writer = new StreamWriter(outPath))
+            {
+                foreach(var word in myDict)
+                {
+                    writer.WriteLine("{0}   {1}", word.Key, word.Value);
+                }
+            }
+            Console.WriteLine("Out path: " + outPath);
+
+            Console.ReadLine();
+
+        }
+    }
+}
